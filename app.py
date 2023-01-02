fb30775583422225b5cb477d3b854c23d91c7221
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-01 23:24:19 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import streamlit as st
+import yfinance as yf
+
+ticker  = yf.Ticker('AAPL')
+
+@st.cache
+def get_df() -> pd.DataFrame:
+    ticker = yf.Ticker('AAPL')
+    df = ticker.history(period='10y')
+    return df
+df = get_df()
+
+st.title('site')
+st.dataframe(df.tail(1000))
+
