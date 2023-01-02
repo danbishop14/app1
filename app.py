7bb87a4dd912bc1543614f7afe3a3b30d90db649
danbishop14 <69700884+danbishop14@users.noreply.github.com>
2023-01-02 00:17:40 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,19 @@ import pandas as pd
 import streamlit as st
 import yfinance as yf
 
-ticker  = yf.Ticker('AAPL')
+
+st.title('simple_test_site')
+
+#Full Data Caching
+url = "https://redfin-public-data.s3.us-west-2.amazonaws.com/redfin_market_tracker/zip_code_market_tracker.tsv000.gz"
+@st.cache
+def get_data() -> pd.DataFrame:
+    return pd.read_csv(url, compression='gzip', sep='\t')
+df = get_data()
+
+st.dataframe(df.tail(1000))
+
+# ticker  = yf.Ticker('AAPL')
 
 # #Full Data Caching
 # @st.cache
@@ -12,13 +24,13 @@ ticker  = yf.Ticker('AAPL')
 # df1 = get_data()
 
 
-@st.cache
-def get_df() -> pd.DataFrame:
-    ticker = yf.Ticker('AAPL')
-    df = ticker.history(period='10y')
-    return df
-df = get_df()
+# @st.cache
+# def get_df() -> pd.DataFrame:
+#     ticker = yf.Ticker('AAPL')
+#     df = ticker.history(period='10y')
+#     return df
+# df = get_df()
 
-st.title('site')
-st.dataframe(df.tail(400))
+# st.title('site')
+# st.dataframe(df.tail(400))
 
